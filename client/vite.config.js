a475788728e990aa500a98df04258fd6a9a09206
Юрчик <88848940+Yrchik07@users.noreply.github.com>
2024-08-08 20:56:52 +0300

    pre-setting the structure

---
+++ client/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
+});
+
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api': 'http://localhost:5000',
+//     },
+//   },
+// });
